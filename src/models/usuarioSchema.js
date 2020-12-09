0837008e44d57497f4dd49e8e5e40ea37b8a3f4f
camilabulcao <camilabulcao5@gmail.com>
2020-12-08 22:21:40 -0300

    inserção do bcrypt e da data de criação

--- src/models/usuarioSchema.js
+++ src/models/usuarioSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require ("mongoose")
 const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs')
 
 const usuarioSchema =  new Schema ({
     _id: {
@@ -17,12 +18,27 @@ const usuarioSchema =  new Schema ({
     }, 
     telefone:{
         type: String,
-        required:true
+        required:true, 
+        unique: true, 
+        lowercase: true //para forçar tudo ser minusculo
     }, 
     senha:{
         type: String, 
-        required: true
+        required: true, 
+        select: false //para não vim a informação da senha quando buscarmos na base de dados
+
+    }, 
+    createdAt:{
+        type: Date,
+        default:Date.now
     }
+})
+
+usuarioSchema.pre('save', async function(next) {
+    const hash = await bcrypt.hash(this.senha, 10);
+    this.senha = hash; 
+
+    next();
 })
 
 const usuarioCollections = mongoose.model('usuario', usuarioSchema)
